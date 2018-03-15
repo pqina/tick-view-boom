20832145388bc179250a98b3273a846fcfee2ee1
Rik Schennink <rikschennink@gmail.com>
2018-03-15 10:18:23 +0100

    fix problem where boom would not load

--- tick.view.boom.global.js
+++ tick.view.boom.global.js
@@ -1,8 +1,11 @@
+/* eslint-disable */
+
 /*
- * Tick v1.0.1 - Counters Made Easy
- * Copyright (c) 2017 PQINA - http://tickcounterplugin.com
+ * Tick v1.6.1 - Counters Made Easy
+ * Copyright (c) 2018 PQINA - http://tickcounterplugin.com
  */
 (function(root, undefined) {
+	'use strict';
 
 	// only create tick extensions queue if not already available
 	if (!root.Tick) {
@@ -17,111 +20,112 @@
 'use strict';
 
 var index = (function () {
-	var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : API,
-	    request = _ref.Data.request,
-	    performance = _ref.Date.performance,
-	    _ref$View = _ref.View,
-	    rooter = _ref$View.rooter,
-	    destroyer = _ref$View.destroyer,
-	    drawer = _ref$View.drawer,
-	    updater = _ref$View.updater,
-	    styler = _ref$View.styler;
+    var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : API,
+        request = _ref.Data.request,
+        performance = _ref.Date.performance,
+        _ref$View = _ref.View,
+        rooter = _ref$View.rooter,
+        destroyer = _ref$View.destroyer,
+        drawer = _ref$View.drawer,
+        updater = _ref$View.updater,
+        styler = _ref$View.styler;
 
-	var AudioContext = typeof window !== 'undefined' ? window.AudioContext || window.webkitAudioContext : null;
-	var context = AudioContext ? new AudioContext() : null;
+    var AudioContext = typeof window !== 'undefined' ? window.AudioContext || window.webkitAudioContext : null;
+    var context = AudioContext ? new AudioContext() : null;
 
-	var getBuffer = function getBuffer(context, data, cb) {
+    var getBuffer = function getBuffer(context, data, cb) {
+        context.decodeAudioData(data, function (buffer) {
+            cb(buffer);
+        });
+    };
 
-		context.decodeAudioData(data, function (buffer) {
-			cb(buffer);
-		});
-	};
+    var preload = function preload(context, files, cb) {
+        (Array.isArray(files) ? files : [files]).forEach(function (file) {
+            // load source files
+            request(file, function (res) {
+                // handle success response
+                getBuffer(context, res, cb);
+            }, function (err) {
+                // handle error
+            }, function (xhr) {
+                // dress up xhr
+                xhr.responseType = 'arraybuffer';
+            });
+        });
+    };
 
-	var preload = function preload(context, files, cb) {
+    var createSound = function createSound(context, buffer) {
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        return source;
+    };
 
-		(Array.isArray(files) ? files : [files]).forEach(function (file) {
+    var draw = function draw(state) {
+        if (!context || state.style.sample === null || !state.style.sample.length) {
+            return;
+        }
 
-			// load source files
-			request(file, function (res) {
-				// handle success response
-				getBuffer(context, res, cb);
-			}, function (err) {
-				// handle error
-			}, function (xhr) {
-				// dress up xhr
-				xhr.responseType = 'arraybuffer';
-			});
-		});
-	};
+        if (!state.audioLoaded) {
+            // create volume node
+            state.volume = context.createGain();
+            state.volume.connect(context.destination);
 
-	var createSound = function createSound(context, buffer) {
-		var source = context.createBufferSource();
-		source.buffer = buffer;
-		return source;
-	};
+            // load audio
+            preload(context, state.style.sample, function (buffer) {
+                if (!buffer || state.buffer) {
+                    return;
+                }
+                state.buffer = buffer;
+                state.audioLoaded = true;
+            });
 
-	var draw = function draw(state) {
+            // don't play sounds on first draw
+            return;
+        }
 
-		if (!context || state.style.sample === null || !state.style.sample.length) {
-			return;
-		}
+        // don't play sound if value has not changed
+        if (state.value === state.lastValue) {
+            return;
+        }
 
-		if (!state.audioLoaded) {
+        // play sound
+        var sound = createSound(context, state.buffer);
+        sound.connect(state.volume);
+        state.volume.gain.value = state.style.volume;
 
-			// create volume node
-			state.volume = context.createGain();
-			state.volume.connect(context.destination);
+        var currentDraw = performance();
+        var dist = currentDraw - state.lastDraw;
+        if (dist < state.style.pitchThreshold) {
+            state.currentPitch = Math.min(state.style.pitchMax, state.currentPitch + state.style.pitchStep);
+        } else {
+            state.currentPitch = 1;
+        }
+        state.lastDraw = currentDraw;
+        sound.playbackRate.value = state.currentPitch;
+        sound.start(context.currentTime);
 
-			// load audio
-			preload(context, state.style.sample, function (buffer) {
-				if (!buffer || state.buffer) {
-					return;
-				}
-				state.buffer = buffer;
-				state.audioLoaded = true;
-			});
+        // remember this value
+        state.lastValue = state.value;
+    };
 
-			// don't play sounds on first draw
-			return;
-		}
+    return function (root) {
+        var state = {
+            audioLoaded: false,
+            buffer: null,
+            lastDraw: 0,
+            currentPitch: 1,
+            volume: null,
+            lastValue: null
+        };
 
-		// play sound
-		var sound = createSound(context, state.buffer);
-		sound.connect(state.volume);
-		state.volume.gain.value = state.style.volume;
-
-		var currentDraw = performance();
-		var dist = currentDraw - state.lastDraw;
-		if (dist < state.style.pitchThreshold) {
-			state.currentPitch = Math.min(state.style.pitchMax, state.currentPitch + state.style.pitchStep);
-		} else {
-			state.currentPitch = 1;
-		}
-		state.lastDraw = currentDraw;
-		sound.playbackRate.value = state.currentPitch;
-		sound.start(context.currentTime);
-	};
-
-	return function (root) {
-
-		var state = {
-			audioLoaded: false,
-			buffer: null,
-			lastDraw: 0,
-			currentPitch: 1,
-			volume: null
-		};
-
-		drawer(state, draw);
-
-		return Object.assign({}, rooter(state, root, 'audio'), updater(state), styler(state, {
-			sample: null,
-			volume: .5, // range from 0 to 1,
-			pitchThreshold: 1000,
-			pitchStep: .05,
-			pitchMax: 1.35
-		}), destroyer(state));
-	};
+        return Object.assign({}, rooter(state, root, 'boom'), updater(state), styler(state, {
+            sample: null,
+            volume: 0.5, // range from 0 to 1,
+            pitchThreshold: 1000,
+            pitchStep: 0.05,
+            pitchMax: 1.35
+        }), destroyer(state), drawer(state, draw));
+    };
 });
 
 module.exports = index;
